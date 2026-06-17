4acf1fe58175e45c122802d403cf98b4bfacbf1e
AmreetaSurana <amreetasurana@gmail.com>
2026-06-17 11:56:08 +0530

    changes in the config file to deploy on streamlit cloud

--- app/config/config.py
+++ app/config/config.py
@@ -1,8 +1,7 @@
 from dotenv import load_dotenv
 import os
-
-
+import streamlit as st
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-API_MODEL = os.getenv("API_MODEL")
+API_KEY = os.getenv("API_KEY") or st.secrets["API_KEY"]
+API_MODEL = os.getenv("API_KEY") or st.secrets["API_MODEL"]
